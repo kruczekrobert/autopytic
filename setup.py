97fe1ff4c801be4173b0c3ffb472ac5ce2080c18
Robert Kruczek <dev@roberts-mbp.home>
2021-03-18 19:22:53 +0100

    Stable v1.0.0

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='autopytic',  
+     version='0.1',
+     author="Robert Kruczek",
+     author_email="kruczeknb1@gmail.com",
+     description="A autopytic package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/kruczekrobert/autopytic",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
